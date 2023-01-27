095bc4f9e66367aedffafbe5a0f6829b973ba84f
Laremies <laurensvanderhelm@live.com>
2023-01-27 19:27:05 +0200

    added unit tests to 'Identify' class and refactored method 'parse'

--- src/main/scala/Identify.scala
+++ src/main/scala/Identify.scala
@@ -3,38 +3,37 @@ import scala.collection.mutable.Map
 
 class Identify(val uri: String):
   private val scheme: String = "visma-identity"
-  private val allowedPaths: Vector[String] = Vector("login", "confirm", "sign")
   private val parameters: Map[String, String] = Map()
   private var path = ""
 
   private def parse(): Unit =
     val uriToParse = new URI(this.uri)
     if uriToParse.getScheme != this.scheme then
-      throw new IllegalArgumentException("Invalid URI sheme")
-
-    this.path = uriToParse.getAuthority
-    if !allowedPaths.contains(this.path) then
-      throw new IllegalArgumentException("Invalid path")
+      throw new IllegalArgumentException("Invalid URI scheme")
 
     this.parameters ++= uriToParse.getQuery
                                   .split("&")
                                   .map(_.split("="))
                                   .map(arr => arr(0) -> arr(1)).toMap
 
+    val checkSource: Boolean = this.parameters.contains("source")
+    this.path = uriToParse.getAuthority
     path match
       case "login" =>
-        if !this.parameters.contains("source") then
+        if !checkSource then
           throw new IllegalArgumentException("Missing source parameter for login path")
       case "confirm" =>
-        if !this.parameters.contains("source") || !this.parameters.contains("paymentnumber") then
+        if !checkSource || !this.parameters.contains("paymentnumber") then
           throw new IllegalArgumentException("Missing source or 'paymentnumber' parameter for confirm path")
         try
           this.parameters("paymentnumber").toInt
         catch
-          case _: NumberFormatException => throw new IllegalArgumentException("Payment must be an integer")
+          case _: NumberFormatException => throw new IllegalArgumentException("Payment number must be an integer")
       case "sign" =>
-        if !this.parameters.contains("source") || !this.parameters.contains("documentid") then
+        if !checkSource || !this.parameters.contains("documentid") then
           throw new IllegalArgumentException("Missing source or 'documentid' parameter for sign path")
+      case _ => throw new IllegalArgumentException("Invalid path")
+
   end parse
 
   parse()
@@ -45,4 +44,3 @@ class Identify(val uri: String):
 
 
 end Identify
-
--- src/test/scala/IdentifySpec.scala
+++ src/test/scala/IdentifySpec.scala
@@ -1,10 +1,42 @@
-import collection.mutable.Stack
 import org.scalatest._
 import flatspec._
 import matchers._
 
-class IdentifySpec extends AnyFlatSpec with should .Matchers:
+class IdentifySpec extends AnyFlatSpec with should.Matchers:
 
+  "Identify" should "return correct path and parameters with 'login'" in {
+    val identifyLogin = new Identify("visma-identity://login?source=severa")
+    identifyLogin.getPath should be ("login")
+    identifyLogin.getParameters should be (Map[String, String]("source" -> "severa"))
+  }
+
+  it should "return correct path and parameters with 'confirm'" in {
+    val indentifyConfirm = new Identify("visma-identity://confirm?source=netvisor&paymentnumber=102226")
+    indentifyConfirm.getPath should be ("confirm")
+    indentifyConfirm.getParameters should be (Map[String, String]("source" -> "netvisor", "paymentnumber" -> "102226"))
+  }
+
+  it should "return correct path and parameters with 'sign'" in {
+    val indentifyConfirm = new Identify("visma-identity://sign?source=vismasign&documentid=105ab44")
+    indentifyConfirm.getPath should be("sign")
+    indentifyConfirm.getParameters should be(Map[String, String]("source" -> "vismasign", "documentid" -> "105ab44"))
+  }
+
+  it should "throw correct error if schema is wrong" in {
+    val expectedMessage = "Invalid URI scheme"
+    the [IllegalArgumentException] thrownBy(new Identify("vism-identity://login?source=severa")) should have message expectedMessage
+  }
+
+  it should "throw correct error if path is wrong" in {
+    val expectedMessage = "Invalid path"
+    the[IllegalArgumentException] thrownBy (new Identify("visma-identity://loginn?source=severa")) should have message expectedMessage
+  }
+
+  it should "throw correct error if paymentnumber is not an integer" in {
+    val expectedMessage = "Payment number must be an integer"
+    the[IllegalArgumentException] thrownBy (new Identify("visma-identity://confirm?source=netvisor&paymentnumber=102.5")) should have message expectedMessage
+    the[IllegalArgumentException] thrownBy (new Identify("visma-identity://confirm?source=netvisor&paymentnumber=hundredone")) should have message expectedMessage
+  }
 
 
 end IdentifySpec
