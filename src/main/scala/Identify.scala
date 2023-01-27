3a3b3e1d68b85476399c39c9008de52137718a3b
Laremies <laurensvanderhelm@live.com>
2023-01-27 17:57:14 +0200

    Identify class parses uri

---
+++ src/main/scala/Identify.scala
@@ -0,0 +1,48 @@
+import java.net.URI
+import scala.collection.mutable.Map
+
+class Identify(val uri: String):
+  private val scheme: String = "visma-identity"
+  private val allowedPaths: Vector[String] = Vector("login", "confirm", "sign")
+  private val parameters: Map[String, String] = Map()
+  private var path = ""
+
+  private def parse(): Unit =
+    val uriToParse = new URI(this.uri)
+    if uriToParse.getScheme != this.scheme then
+      throw new IllegalArgumentException("Invalid URI sheme")
+
+    this.path = uriToParse.getPath
+    if !allowedPaths.contains(path) then
+      throw new IllegalArgumentException("Invalid path")
+
+    this.parameters ++= uriToParse.getQuery
+                                  .split("&")
+                                  .map(_.split("="))
+                                  .map(arr => arr(0) -> arr(1)).toMap
+
+    path match
+      case "login" =>
+        if !this.parameters.contains("source") then
+          throw new IllegalArgumentException("Missing source parameter for login path")
+      case "confirm" =>
+        if !this.parameters.contains("source") || !this.parameters.contains("paymentnumber") then
+          throw new IllegalArgumentException("Missing source or 'paymentnumber' parameter for confirm path")
+        try
+          this.parameters("paymentnumber").toInt
+        catch
+          case _: NumberFormatException => throw new IllegalArgumentException("Payment must be an integer")
+      case "sign" =>
+        if !this.parameters.contains("source") || !this.parameters.contains("documentid") then
+          throw new IllegalArgumentException("Missing source or 'documentid' parameter for sign path")
+  end parse
+
+  parse()
+
+  def getPath: String = path
+
+  def getParameters: Map[String, String] = parameters
+
+
+end Identify
+
